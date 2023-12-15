1142b05004922857cd0e262fddc4684cb988b0c5
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-16 03:55:25 +0800

    feat: histogram

---
+++ src/histogram.rs
@@ -0,0 +1,52 @@
+use std::path::PathBuf;
+
+use clap::Args;
+use plotly::{common::Title, layout::Axis, Histogram, Layout, Plot, Trace};
+use polars::{frame::DataFrame, series::Series};
+
+use crate::io::read_df_file;
+
+#[derive(Debug, Clone, Args)]
+pub struct HistogramArgs {
+    input: PathBuf,
+    #[clap(short, long, default_value = "x")]
+    x: Vec<String>,
+    #[clap(short, long)]
+    output: Option<PathBuf>,
+}
+
+impl HistogramArgs {
+    pub fn run(self) -> anyhow::Result<()> {
+        let df = read_df_file(self.input, None)?;
+        let plot = plot(df.collect()?, &self.x)?;
+        match self.output {
+            Some(output) => plot.write_html(output),
+            None => plot.show(),
+        }
+        Ok(())
+    }
+}
+
+fn plot(df: DataFrame, x: &[String]) -> anyhow::Result<Plot> {
+    let mut plot = Plot::new();
+
+    for x in x {
+        let x = df.column(x)?;
+        let trace = trace(x)?;
+        plot.add_trace(trace);
+    }
+
+    let mut layout = Layout::default().y_axis(Axis::default().title(Title::new("count")));
+    if x.len() == 1 {
+        layout = layout.x_axis(Axis::default().title(Title::new(x.first().unwrap())));
+    }
+    plot.set_layout(layout);
+    Ok(plot)
+}
+
+fn trace(x: &Series) -> anyhow::Result<Box<dyn Trace>> {
+    let name = x.name();
+    let x = x.to_float()?.f64()?.cont_slice()?.to_vec();
+    let trace = Histogram::new(x).name(name);
+    Ok(trace)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
 use clap::Subcommand;
+use histogram::HistogramArgs;
 use scatter::ScatterArgs;
 
+pub mod histogram;
 pub mod io;
 pub mod scatter;
 
 #[derive(Debug, Clone, Subcommand)]
 pub enum Command {
     Scatter(ScatterArgs),
+    Histogram(HistogramArgs),
 }
 
 impl Command {
     pub fn run(self) -> anyhow::Result<()> {
         match self {
             Command::Scatter(args) => args.run(),
+            Command::Histogram(args) => args.run(),
         }
     }
 }
