b43493828cf421cfcf4474ab86642501284ae556
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-17 00:24:46 +0800

    feat: scatter groups

--- src/numeral/scatter.rs
+++ src/numeral/scatter.rs
@@ -1,8 +1,16 @@
-use std::path::PathBuf;
+use std::{borrow::Cow, path::PathBuf};
 
+use anyhow::Context;
 use clap::Args;
 use plotly::{common::Title, layout::Axis, Layout, Plot, Scatter, Trace};
-use polars::{frame::DataFrame, series::Series};
+use polars::{
+    frame::DataFrame,
+    lazy::{
+        dsl::{col, lit},
+        frame::IntoLazy,
+    },
+    series::Series,
+};
 
 use crate::io::{output_plot, read_df_file};
 
@@ -14,27 +22,63 @@ pub struct ScatterArgs {
     #[clap(short, long, default_value = "y")]
     y: Vec<String>,
     #[clap(short, long)]
+    group: Option<String>,
+    #[clap(short, long)]
     output: Option<PathBuf>,
 }
 
 impl ScatterArgs {
     pub fn run(self) -> anyhow::Result<()> {
         let df = read_df_file(self.input, None)?;
-        let plot = plot(df.collect()?, &self.x, &self.y)?;
+        let plot = plot(df.collect()?, &self.x, &self.y, self.group.as_deref())?;
         output_plot(plot, self.output.as_deref())?;
         Ok(())
     }
 }
 
-fn plot(df: DataFrame, x: &str, y: &[String]) -> anyhow::Result<Plot> {
+fn plot(df: DataFrame, x: &str, y: &[String], group: Option<&str>) -> anyhow::Result<Plot> {
     let mut plot = Plot::new();
     let x_title = Title::new(x);
 
-    let x = df.column(x).ok();
-    for y in y {
-        let y = df.column(y)?;
-        let trace = trace(x, y)?;
-        plot.add_trace(trace);
+    let group_names = match group {
+        Some(group) => {
+            let group_column = df.column(group)?;
+            let group_columns = group_column.unique()?;
+            let group_names = group_columns
+                .utf8()?
+                .into_iter()
+                .map(|x| x.map(|s| s.to_string()).context("No string in group"))
+                .collect::<Result<Vec<_>, _>>()?;
+            Some((group, group_names))
+        }
+        None => None,
+    };
+
+    match group_names {
+        Some((group, group_names)) => {
+            let lazy = df.lazy();
+            for group_name in group_names {
+                let df = lazy.clone();
+                let df = df
+                    .clone()
+                    .filter(col(group).eq(lit(&*group_name)))
+                    .collect()?;
+                let x = df.column(x).ok();
+                for y in y {
+                    let y = df.column(y)?;
+                    let trace = trace(x, y, Some(&group_name))?;
+                    plot.add_trace(trace);
+                }
+            }
+        }
+        None => {
+            let x = df.column(x).ok();
+            for y in y {
+                let y = df.column(y)?;
+                let trace = trace(x, y, None)?;
+                plot.add_trace(trace);
+            }
+        }
     }
 
     let mut layout = Layout::default().x_axis(Axis::default().title(x_title));
@@ -45,8 +89,11 @@ fn plot(df: DataFrame, x: &str, y: &[String]) -> anyhow::Result<Plot> {
     Ok(plot)
 }
 
-fn trace(x: Option<&Series>, y: &Series) -> anyhow::Result<Box<dyn Trace>> {
-    let name = y.name();
+fn trace(x: Option<&Series>, y: &Series, group: Option<&str>) -> anyhow::Result<Box<dyn Trace>> {
+    let name: Cow<str> = match group {
+        Some(group) => format!("{}:{}", group, y.name()).into(),
+        None => y.name().into(),
+    };
     let x = match x {
         Some(x) => x.to_float()?.f64()?.cont_slice()?.to_vec(),
         None => (0..y.len()).map(|x| (x + 1) as f64).collect(),
