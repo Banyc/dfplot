b548dc3ffdd5dab6dd5dfc9f9ed667c043ef1513
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-17 23:17:03 +0800

    feat: scatter plot groups

--- src/numeral/scatter.rs
+++ src/numeral/scatter.rs
@@ -2,17 +2,10 @@ use std::{borrow::Cow, path::PathBuf};
 
 use clap::Args;
 use plotly::{common::Title, layout::Axis, Layout, Plot, Scatter, Trace};
-use polars::{
-    frame::DataFrame,
-    lazy::{
-        dsl::{col, lit},
-        frame::IntoLazy,
-    },
-    series::Series,
-};
+use polars::{frame::DataFrame, lazy::frame::IntoLazy, series::Series};
 
 use crate::{
-    df::category_names,
+    group::Groups,
     io::{output_plot, read_df_file},
 };
 
@@ -24,7 +17,7 @@ pub struct ScatterArgs {
     #[clap(short, long, default_value = "y")]
     y: Vec<String>,
     #[clap(short, long)]
-    group: Option<String>,
+    group: Option<Vec<String>>,
     #[clap(short, long)]
     output: Option<PathBuf>,
 }
@@ -32,40 +25,37 @@ pub struct ScatterArgs {
 impl ScatterArgs {
     pub fn run(self) -> anyhow::Result<()> {
         let df = read_df_file(self.input, None)?;
-        let plot = plot(df.collect()?, &self.x, &self.y, self.group.as_deref())?;
+        let plot = plot(df.collect()?, &self.x, &self.y, self.group)?;
         output_plot(plot, self.output.as_deref())?;
         Ok(())
     }
 }
 
-fn plot(df: DataFrame, x: &str, y: &[String], group: Option<&str>) -> anyhow::Result<Plot> {
+fn plot(df: DataFrame, x: &str, y: &[String], groups: Option<Vec<String>>) -> anyhow::Result<Plot> {
     let mut plot = Plot::new();
     let x_title = Title::new(x);
 
-    let group_names = match group {
-        Some(group) => {
-            let group_names = category_names(&df, group)?;
-            Some((group, group_names))
-        }
+    let groups = match groups {
+        Some(groups) => Some(Groups::from_df(&df, groups)?),
         None => None,
     };
 
-    match group_names {
-        Some((group, group_names)) => {
-            let lazy = df.lazy();
-            for group_name in group_names {
-                let df = lazy.clone();
-                let df = df
-                    .clone()
-                    .filter(col(group).eq(lit(&*group_name)))
-                    .collect()?;
+    match groups {
+        Some(groups) => {
+            groups.for_each_product(df.lazy(), |df, groups| {
+                let groups = groups
+                    .into_iter()
+                    .map(|pair| pair.category)
+                    .collect::<Vec<_>>();
+                let df = df.collect()?;
                 let x = df.column(x).ok();
                 for y in y {
                     let y = df.column(y)?;
-                    let trace = trace(x, y, Some(&group_name))?;
+                    let trace = trace(x, y, Some(&groups))?;
                     plot.add_trace(trace);
                 }
-            }
+                Ok(())
+            })?;
         }
         None => {
             let x = df.column(x).ok();
@@ -85,11 +75,16 @@ fn plot(df: DataFrame, x: &str, y: &[String], group: Option<&str>) -> anyhow::Re
     Ok(plot)
 }
 
-fn trace(x: Option<&Series>, y: &Series, group: Option<&str>) -> anyhow::Result<Box<dyn Trace>> {
-    let name: Cow<str> = match group {
-        Some(group) => format!("{}:{}", group, y.name()).into(),
+fn trace(
+    x: Option<&Series>,
+    y: &Series,
+    groups: Option<&[&str]>,
+) -> anyhow::Result<Box<dyn Trace>> {
+    let name: Cow<str> = match groups {
+        Some(groups) => format!("{:?}:{}", groups, y.name()).into(),
         None => y.name().into(),
     };
+
     let x = match x {
         Some(x) => x.to_float()?.f64()?.cont_slice()?.to_vec(),
         None => (0..y.len()).map(|x| (x + 1) as f64).collect(),
